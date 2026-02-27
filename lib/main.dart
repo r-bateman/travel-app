1a0db407392d17a2f63d093668dc0e4bc046114b
becca-dev <beccab>
2026-02-26 21:33:23 -0700

    re-added google services because you HAVE to add it through xcode for some reason.... additionally added all the set up for firebase stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//
-//   await Firebase.initializeApp();
-//
-//   runApp(const MyApp());
-// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
+
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
