5aa4d67b03cee427fdd8b7f9c1f1a11b62afb22d
becca-dev <beccab>
2026-02-28 12:16:34 -0700

    add login page and test login page (to be renamed later prob)

---
+++ lib/pages/test_login_credentials.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/home_page.dart';
+import 'package:travel_app/pages/login_page.dart';
+
+
+class TestLoginCredentials extends StatelessWidget {
+  const TestLoginCredentials({ Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
